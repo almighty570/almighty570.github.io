37eca4c48ad8eb581b1313a18d95667893568733
Roshan <arobce@gmail.com>
2020-03-17 12:30:04 +0545

    Adds RadioGroup

---
+++ src/components/core/RadioGroup.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="form-group">
+    <div class="radio-wrapper">
+      <div v-for="option in options" :key="option" class="radio-item">
+        <input
+          type="radio"
+          ref="input"
+          :id="option.value"
+          :value="option.value"
+          :name="name"
+          v-model="radio_value"
+          @input="updateValue()"
+        />
+        <label :for="option.value">{{option.label}}</label>
+      </div>
+    </div>
+  </div>
+</template>``
+
+<script>
+export default {
+  name: "RadioGroup",
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+
+    name: {
+      type: String,
+      required: true
+    }
+  },
+
+  data: {
+    radio_value: null
+  },
+
+  methods: {
+    updateValue() {
+      this.$emit("input", this.radio_value);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.radio-wrapper {
+  display: flex;
+  justify-content: space-between;
+}
+
+.radio-wrapper input {
+  margin-right: 5px;
+  width: 20px;
+  height: 1em;
+}
+</style>
--- src/views/Seller/Dashboard.vue
+++ src/views/Seller/Dashboard.vue
@@ -74,10 +74,28 @@
 
         <div class="row mt-4">
           <div class="col-12">
-            <TextBox id="unique" type="text" v-model="val" label="INput here" placeholder="shh....." />
+            <TextBox
+              id="unique"
+              type="text"
+              v-model="val"
+              label="INput here"
+              placeholder="shh....."
+            />
           </div>
           <div class="col-12">
-            <NumberField id="number-field" v-model="numberFieldVal" label="Enter Number Here" placeholder="2050" />
+            <NumberField
+              id="number-field"
+              v-model="number_field_val"
+              label="Enter Number Here"
+              placeholder="2050"
+            />
+          </div>
+          <div class="col-12">
+            <RadioGroup
+              :options="[{value:'male' , label:'Male'},{value:'female' , label:'Female'},{value:'others' , label:'Others'}]"
+              name="gender"
+              v-model="radio_field"
+            />
           </div>
         </div>
       </div>
@@ -88,17 +106,20 @@
 <script>
 import TextBox from "@/components/core/TextBox";
 import NumberField from "@/components/core/NumberField";
+import RadioGroup from "@/components/core/RadioGroup";
 
 export default {
   name: "Dashbaord",
   components: {
     TextBox,
-    NumberField
+    NumberField,
+    RadioGroup
   },
   data() {
     return {
       val: null,
-      numberFieldVal: null
+      number_field_val: null,
+      radio_field: null
     };
   }
 };
