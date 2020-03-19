2b71dee40863dfb28e11c21a69e73115603adc8a
Roshan <arobce@gmail.com>
2020-03-19 13:29:55 +0545

    Adds Dashboard Variation

--- src/views/Seller/Dashboard.vue
+++ src/views/Seller/Dashboard.vue
@@ -223,8 +223,130 @@
                 </div>
               </Card>
             </div>
+            
 
             <div class="col-6">
+              <Card custom_class="card-danger" id="cod-details-card">
+                <h2 slot="title">COD</h2>
+                <div slot="body" class="info-box-wrapper">
+                  <div class="row">
+                    <div class="col-12">
+                      <SmallBox>
+                        <div slot="inner">
+                          <h3>1500</h3>
+                          <p>COD Value</p>
+                        </div>
+                        <i slot="icon" class="icon far fa-box-usd"></i>
+                      </SmallBox>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </div>
+          </div>
+        </div>
+
+        <h2 class="text-dark">Dashboard Long Card</h2>
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <Card custom_class="card-primary" id="products-details-card">
+                <h2 slot="title">Products</h2>
+                <div slot="body" class="info-box-wrapper">
+                  <div class="row">
+                    <div class="col-6">
+                      <SmallBox>
+                        <div slot="inner">
+                          <h3>150</h3>
+                          <p>Products</p>
+                        </div>
+                        <i slot="icon" class="icon fal fa-box-full"></i>
+                      </SmallBox>
+                    </div>
+                    <div class="col-6">
+                      <SmallBox>
+                        <div slot="inner">
+                          <h3>2323</h3>
+                          <p>Items</p>
+                        </div>
+                        <i slot="icon" class="icon far fa-sitemap"></i>
+                      </SmallBox>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </div>
+            <div class="col-12">
+              <Card custom_class="card-warning" id="shipping-details-card">
+                <h2 slot="title">Shipping</h2>
+                <div slot="body" class="info-box-wrapper">
+                  <div class="row">
+                    <div class="col-6">
+                      <SmallBox>
+                        <div slot="inner">
+                          <h3>12</h3>
+                          <p>To Ship</p>
+                        </div>
+                        <i slot="icon" class="icon far fa-box-alt"></i>
+                      </SmallBox>
+                    </div>
+                    <div class="col-6">
+                      <SmallBox>
+                        <div slot="inner">
+                          <h3>8</h3>
+                          <p>In Transit</p>
+                        </div>
+                        <i slot="icon" class="icon far fa-dolly-flatbed-alt"></i>
+                      </SmallBox>
+                    </div>
+                    <div class="col-6">
+                      <SmallBox>
+                        <div slot="inner">
+                          <h3>112</h3>
+                          <p>Delivered</p>
+                        </div>
+                        <i slot="icon" class="icon far fa-box-check"></i>
+                      </SmallBox>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <Card custom_class="card-success" id="orders-details-card">
+                <h2 slot="title">Orders</h2>
+                <div slot="body" class="info-box-wrapper">
+                  <div class="row">
+                    <div class="col-6">
+                      <SmallBox>
+                        <div slot="inner">
+                          <h3>150</h3>
+                          <p>New Orders</p>
+                        </div>
+                        <i slot="icon" class="icon fal fa-dolly"></i>
+                      </SmallBox>
+                    </div>
+                    <div class="col-6">
+                      <SmallBox>
+                        <div slot="inner">
+                          <h3>
+                            53
+                            <sup style="font-size: 20px">%</sup>
+                          </h3>
+                          <p>Growth</p>
+                        </div>
+                        <i slot="icon" class="icon fal fa-chart-line"></i>
+                      </SmallBox>
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </div>
+            
+
+            <div class="col-12">
               <Card custom_class="card-danger" id="cod-details-card">
                 <h2 slot="title">COD</h2>
                 <div slot="body" class="info-box-wrapper">
