37994f4fa09d51eed9dfd30191c2ebf955519bef
Roshan <arobce@gmail.com>
2020-03-25 15:19:16 +0545

    Adds Screen Transition Register

---
+++ src/components/derived/OTPField.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="otp-wrapper">
+    <NumberField id="phone-textbox" class="otp-field"  v-model="otp1" />
+    <NumberField id="phone-textbox" class="otp-field"  v-model="otp2" />
+    <NumberField id="phone-textbox" class="otp-field"  v-model="otp3" />
+    <NumberField id="phone-textbox" class="otp-field"  v-model="otp4" />
+    <NumberField id="phone-textbox" class="otp-field"  v-model="otp5" />
+  </div>
+</template>
+
+<script>
+import NumberField from "@/components/core/NumberField";
+
+export default {
+  name: "OTPField",
+  components: {
+    NumberField
+  },
+  mounted() {
+
+  },
+  data() {
+    return {
+      otp1: null,
+      otp2: null,
+      otp3: null,
+      otp4: null,
+      otp5: null
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped>
+.otp-wrapper {
+  display: flex;
+  justify-content: space-between;
+}
+.otp-field {
+  width: 40px;
+}
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,24 +1,54 @@
 <template>
   <div class="register">
-    <div class="auth-box ">
+    <div class="auth-box">
       <div class="site-logo">
         <a href>
           <span class="font-weight-bold">Pro</span>Ship
         </a>
       </div>
       <div class="auth-box__body card">
-                <Stepper :steps="3" :currentStepIndex="2"/>
-        <p class="auth-box-body__msg">Register to start your session</p>
-        <form @submit="handleRegister">
-          <TextBox type="text" id="name-textbox" placeholder="Name" v-model="name" />
-          <NumberField id="phone-textbox" placeholder="Phone Number" v-model="number" />
+        <div class="initial-register" v-if="initialRegister">
+          <Stepper :steps="3" :currentStepIndex="1" />
+          <p class="auth-box-body__msg">Register to start your session</p>
+          <form>
+            <TextBox type="text" id="name-textbox" placeholder="Name" v-model="name" />
+            <NumberField id="phone-textbox" placeholder="Phone Number" v-model="number" />
 
-          <TextBox type="text" id="email-textbox" placeholder="Email" v-model="email" />
+            <TextBox type="text" id="email-textbox" placeholder="Email" v-model="email" />
 
-          <div class="auth-box__cta-wrapper d-flex justify-content-end">
-            <Button id="register-form-button" variant="info" size="md">Register</Button>
+            <div class="auth-box__cta-wrapper d-flex justify-content-end">
+              <Button id="register-form-button" variant="primary" size="md" @click="onRegister">Register</Button>
+            </div>
+          </form>
+        </div>
+
+        <div class="otp-verification" v-if="otpVerification">
+          <p class="auth-box-body__msg">
+            Please type the verification code sent to
+            <span class="font-weight-bold">+977 9843123123</span>
+          </p>
+          <form @submit="handleRegister">
+            <OTPField />
+            <p class="auth-box-body__msg">Didn't recieve the OTP?</p>
+            <p class="auth-box-body__msg">
+              <a href="#" class="otp-resend">Resend OTP</a>
+            </p>
+            <div class="auth-box__cta-wrapper d-flex justify-content-end">
+              <Button id="register-form-button" variant="primary" size="md" @click="onVerification">Verify</Button>
+            </div>
+          </form>
+        </div>
+
+        <div class="final-login-prompt" v-if="finalLogin">
+          <div class="sucess-icon">
+            <i class="fal fa-check-circle"></i>
           </div>
-        </form>
+          <p class="auth-box-body__msg sucessfully-registered-messege">Successfully Registered</p>
+          <p class="auth-box-body__msg">
+            You can
+            <a href="#" class="otp-resend">login now.</a>
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -31,6 +61,7 @@ import CheckBoxGroup from "@/components/core/CheckBoxGroup";
 import NumberField from "@/components/core/NumberField";
 import DatePicker from "@/components/core/DatePicker";
 import Stepper from "@/components/core/Stepper";
+import OTPField from "@/components/derived/OTPField";
 
 export default {
   name: "Home",
@@ -40,7 +71,9 @@ export default {
     CheckBoxGroup,
     NumberField,
     DatePicker,
-    Stepper
+    Stepper,
+    OTPField,
+    
   },
 
   data() {
@@ -50,14 +83,21 @@ export default {
       name: null,
       number: null,
       dateOfBirth: null,
-      confirm_password: null
+      confirm_password: null,
+      finalLogin: false,
+      otpVerification: false,
+      initialRegister: true
     };
   },
 
   methods: {
-    handleRegister(e) {
-      e.preventDefault();
-      this.$router.push({ name: "Seller-Dashboard" });
+    onRegister: function(){
+      this.initialRegister = false;
+      this.otpVerification = true;
+    },
+    onVerification: function(){
+      this.otpVerification = false;
+      this.finalLogin = true;
     }
   }
 };
@@ -65,5 +105,27 @@ export default {
 
 
 <style scoped>
+.otp-resend-msg {
+  text-transform: uppercase;
+  font-weight: bold;
+}
 
+.otp-resend {
+  text-decoration: underline;
+}
+
+.auth-link {
+  text-decoration: underline;
+}
+
+.sucessfully-registered-messege {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.sucess-icon {
+  text-align: center;
+  font-size: 50px;
+  color: #28a745;
+}
 </style>
