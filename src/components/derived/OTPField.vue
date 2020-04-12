fa57c901ff846a96a47e4299220677eccbf72686
Bibhuti <bibhutipd@gmail.com>
2020-04-12 13:35:51 +0545

    Adds number modifier

--- src/components/derived/OTPField.vue
+++ src/components/derived/OTPField.vue
@@ -5,27 +5,27 @@
         id="phone-textbox-1"
         class="otp-field ml-1 mr-1"
         :keyup="movetoNext('phone-textbox-1','phone-textbox-2')"
-        v-model="otp1"
+        v-model.number="otp1"
       />
       <NumberField
         id="phone-textbox-2"
         class="otp-field ml-1 mr-1"
         :keyup="movetoNext('phone-textbox-2','phone-textbox-3')"
-        v-model="otp2"
+        v-model.number="otp2"
       />
       <NumberField
         id="phone-textbox-3"
         class="otp-field ml-1 mr-1"
         :keyup="movetoNext('phone-textbox-3','phone-textbox-4')"
-        v-model="otp3"
+        v-model.number="otp3"
       />
       <NumberField
         id="phone-textbox-4"
         class="otp-field ml-1 mr-1"
         :keyup="movetoNext('phone-textbox-4','phone-textbox-5')"
-        v-model="otp4"
+        v-model.number="otp4"
       />
-      <NumberField id="phone-textbox-5" class="otp-field ml-1 mr-1" v-model="otp5" />
+      <NumberField id="phone-textbox-5" class="otp-field ml-1 mr-1" v-model.number="otp5" />
     </form>
   </div>
 </template>
